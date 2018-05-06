472c12a8a9f221c926383a11144e750141b8b24e
Erick Garcia Godoy <erick_garciagodoy@hotmail.com>
2018-05-06 19:32:44 -0300

    atualização da responsividade

--- Front-End/SobrinhoFront-end Cliente/js/abas.js
+++ Front-End/SobrinhoFront-end Cliente/js/abas.js
@@ -10,6 +10,7 @@ function openAnalista(){
         analista.style.display = "block";    
         btnAnalista.style.boxShadow = "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)";
         btnCliente.style.boxShadow = "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)";
+        analista.style.transition="5s";
 }
 function openCliente(){
     cliente.style.display = "block";
