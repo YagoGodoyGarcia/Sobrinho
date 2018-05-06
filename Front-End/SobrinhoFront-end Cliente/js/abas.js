5aebdf1edeb18213348e5ea7bd195506cde5353e
Erick Garcia Godoy <erick_garciagodoy@hotmail.com>
2018-05-06 14:03:26 -0300

    Adicionando modal / Abas na consulta de cliente e Analista

---
+++ Front-End/SobrinhoFront-end Cliente/js/abas.js
@@ -0,0 +1,19 @@
+var cliente= document.getElementById("buscarCliente");
+var analista= document.getElementById("buscarAnalista");
+var btnCliente = document.getElementById("btnCliente");
+var btnAnalista = document.getElementById("btnAnalista");
+
+
+
+function openAnalista(){
+        cliente.style.display = "none";
+        analista.style.display = "block";    
+        btnAnalista.style.boxShadow = "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)";
+        btnCliente.style.boxShadow = "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)";
+}
+function openCliente(){
+    cliente.style.display = "block";
+    analista.style.display = "none";
+    btnCliente.style.boxShadow = "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)";
+    btnAnalista.style.boxShadow = "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)";
+}
